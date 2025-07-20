7bc6f1630a6b7b8ca8c5a52f5fca201202b1988c
Muhammad-Subhan456 <sbhnamir456@gmail.com>
2025-07-20 11:41:38 +0500

    completed create listing pageUI

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -8,6 +8,7 @@ import {
   updateUser,
 } from "../redux/user/userSlice";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Profile() {
   const fileRef = useRef(null);
@@ -17,7 +18,6 @@ export default function Profile() {
   const dispatch = useDispatch();
   const [formData, setFormData] = useState({});
   const navigate = useNavigate();
-
   // Redirect unauthenticated users
   useEffect(() => {
     if (!currentUser) {
@@ -158,6 +158,10 @@ export default function Profile() {
         <button className="bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80">
           update
         </button>
+        <Link to={"/create-listing"} className="bg-green-700 text-white p-3 rounded-lg uppercase
+        text-center hover:opacity-75 " >
+          Create Listing
+        </Link>
       </form>
       <div className="flex justify-between mt-5">
         <span
