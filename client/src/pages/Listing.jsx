45349555b4b6cdcfdc98dd63169ebd9a60c71dce
Muhammad-Subhan456 <sbhnamir456@gmail.com>
2025-07-23 12:57:12 +0500

    added contact landlord functionality to the listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -145,7 +145,7 @@ export default function Listing() {
                 {listing.furnished ? 'Furnished' : 'Unfurnished'}
               </li>
             </ul>
-
+                  
             {currentUser &&
               listing.userRef !== currentUser._id &&
               !contact && (
